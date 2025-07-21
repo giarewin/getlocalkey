002e044f8f6bd50bada23e107347e94863f17526
giarewin <acc@giare.win>
2025-07-21 19:39:58 +0700

    Update config_flow.py

--- custom_components/getlocalkey/config_flow.py
+++ custom_components/getlocalkey/config_flow.py
@@ -1,19 +1,18 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 
-from .const import DOMAIN, CONF_REGION
-
-DATA_SCHEMA = vol.Schema({
-    vol.Required(CONF_USERNAME): str,
-    vol.Required(CONF_PASSWORD): str,
-    vol.Required(CONF_REGION, default="us"): str,
-})
-
-class GetLocalKeyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+@config_entries.HANDLERS.register("getlocalkey")
+class GetLocalKeyConfigFlow(config_entries.ConfigFlow, domain="getlocalkey"):
     async def async_step_user(self, user_input=None):
-        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input[CONF_USERNAME], data=user_input)
+            return self.async_create_entry(title="Get LocalKey Tuya", data=user_input)
 
-        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("client_id"): str,
+                vol.Required("client_secret"): str,
+                vol.Required("uid"): str,
+                vol.Required("region", default="us"): vol.In(["us", "cn", "eu", "in"])
+            })
+        )
