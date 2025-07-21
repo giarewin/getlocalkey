c1ddb3b9cb286a5c29e5f6dae7166b1b7e7f5e4d
giarewin <acc@giare.win>
2025-07-21 19:41:11 +0700

    Update diagnostics.py

--- custom_components/getlocalkey/diagnostics.py
+++ custom_components/getlocalkey/diagnostics.py
@@ -1,8 +1,1 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
 
-from .const import DOMAIN
-
-async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry):
-    """Return diagnostics data for the config entry."""
-    return hass.data.get(DOMAIN, {}).get(entry.entry_id, {})
